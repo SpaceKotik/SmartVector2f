fcb08f079ee37ab6bbfcd952acd30806b314153d
SpaceKotik <55507150+SpaceKotik@users.noreply.github.com>
2020-03-16 15:43:40 +0300

    Add files via upload

---
+++ include/SmartVector2f.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+
+class eVector2f : public sf::Vector2f {
+public:
+    void multuply(int a);
+};
---
+++ src/SmartVector2f.cpp
@@ -0,0 +1,11 @@
+#include "SmartVector2f.h"
+#include <SFML/Graphics.hpp>
+
+int func() {
+    return 5;
+}
+
+void eVector2f::multuply(int a) {
+    x *= a;
+    y *= a;
+}
